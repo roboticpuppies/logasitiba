4f495ae75db12f16ca39ccc9779908a0cfdb983b
Syaifuddin <syaifuddin@beon.co.id>
2018-11-18 03:53:51 +0700

    finish login;start admin dashboard

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -8,33 +8,36 @@
 	<title>Sistem Peminjaman</title>
 </head>
 <body class="grey darken-4">
-	<nav>
-		<div class="nav-wrapper grey darken-4">
-			<a href="#" class="brand-logo center">Sistem Peminjaman</a>
-			<ul class="left">
-				<li><a href="#" data-target="slide-out" class="sidenav-trigger show-on-large"><i class="material-icons">menu</i></a></li>
-			</ul>
-		</div>
-	</nav>
-	<ul id="slide-out" class="sidenav grey darken-4" style="padding-top: 50px">
-		<li><a class="subheader grey-text"><i class="material-icons grey-text">dashboard</i>Dashboard</a></li>
-		<li><a class="subheader grey-text">Navigation</a></li>
-		<li><a href="#!" class="grey-text"><i class="material-icons grey-text">home</i>Home</a></li>
-		<li><a href="#!" class="grey-text"><i class="material-icons grey-text">people</i>User Management</a></li>
-		<li><a href="#!" class="grey-text"><i class="material-icons grey-text">list</i>Inventory Management</a></li>
-		<li><a class="subheader grey-text">Account</a></li>
-		<li><a href="#!" class="grey-text"><i class="material-icons grey-text">settings</i>Settings</a></li>
-		<li><a href="#!" class="grey-text"><i class="material-icons grey-text">keyboard_backspace</i>Sign Out</a></li>
-	</ul>
+	<header>
+		<nav>
+			<div class="nav-wrapper grey darken-4">
+				<a href="#" class="brand-logo center">Sistem Peminjaman</a>
+				<ul class="left">
+					<li><a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a></li>
+				</ul>
+			</div>
+		</nav>
+		<ul id="slide-out" class="sidenav grey darken-4 sidenav-fixed" style="padding-top: 50px">
+			<li><a class="subheader grey-text"><i class="material-icons grey-text">dashboard</i>Dashboard</a></li>
+			<li><a class="subheader grey-text">Navigation</a></li>
+			<li><a href="#!" class="grey-text"><i class="material-icons grey-text">home</i>Home</a></li>
+			<li><a href="#!" class="grey-text"><i class="material-icons grey-text">people</i>User Management</a></li>
+			<li><a href="#!" class="grey-text"><i class="material-icons grey-text">list</i>Inventory Management</a></li>
+			<li><a class="subheader grey-text">Account</a></li>
+			<li><a href="#!" class="grey-text"><i class="material-icons grey-text">settings</i>Settings</a></li>
+			<li><a href="<?= base_url('auth/logout') ?>" class="grey-text"><i class="material-icons grey-text">keyboard_backspace</i>Sign Out</a></li>
+		</ul>
+	</header>
 
+<main>
 	<div class="container">
-		<h5 class="grey-text">Status</h5>
+		<h5 class="grey-text">Overview</h5>
 		<div class="row">
 			<div class="col s12 m4 l4">
 				<div class="card">
 					<div class="card-content">
 						<span class="card-title">Jumlah barang</span>
-						<h2 class="light">5</h2>
+						<h4 class="light">5</h4>
 					</div>
 				</div>
 			</div>
@@ -42,7 +45,7 @@
 				<div class="card">
 					<div class="card-content">
 						<span class="card-title">Jumlah member</span>
-						<h2 class="light">10</h2>
+						<h4 class="light">10</h4>
 					</div>
 				</div>
 			</div>
@@ -50,12 +53,13 @@
 				<div class="card">
 					<div class="card-content">
 						<span class="card-title">Jumlah peminjam</span>
-						<h2 class="light">3</h2>
+						<h4 class="light">3</h4>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+</main>
 </body>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
